0bd7e48ecd028dcb8a062805acb67d00f8606073
Prasanna agarwal <prasanna23ag@gmail.com>
2026-06-02 10:23:40 +0200

    Update preprocessing.py

--- diffuser/datasets/preprocessing.py
+++ diffuser/datasets/preprocessing.py
@@ -71,32 +71,13 @@ def add_deltas(env):
 
 
 def maze2d_set_terminals(env):
-    # env = load_environment(env) if type(env) == str else env
-    # goal = np.array(env._target)
-    # threshold = 0.5
-
     def _fn(dataset):
-        # xy = dataset["observations"][:, :2]
-        # distances = np.linalg.norm(xy - goal, axis=-1)
-        # at_goal = distances < threshold
-        # timeouts = np.zeros_like(dataset["rewards"])
-
-        # ## timeout at time t iff
-        # ##      at goal at time t and
-        # ##      not at goal at time t + 1
-        # timeouts[:-1] = at_goal[:-1] * ~at_goal[1:]
-
-        # timeout_steps = np.where(timeouts)[0]
-        # path_lengths = timeout_steps[1:] - timeout_steps[:-1]
         path_lengths = np.array([len(dataset["observations"])])
 
         print(
             f"[ utils/preprocessing ] Segmented {env} | {len(path_lengths)} paths | "
             f"min length: {path_lengths.min()} | max length: {path_lengths.max()} | mean length: {path_lengths.mean()}"
         )
-
-        # dataset["timeouts"] = timeouts
-        # dataset["terminals"] = np.zeros_like(dataset["terminals"])
 
         dataset["timeouts"] = np.zeros_like(dataset["rewards"]) 
         dataset["terminals"] = np.zeros_like(dataset["terminals"])
